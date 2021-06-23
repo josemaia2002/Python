dda38231416bae78f3726bdc995378aaa1b2e1f3
josemaia2002 <86317577+josemaia2002@users.noreply.github.com>
2021-06-22 21:35:42 -0300

    Create chaos.py

---
+++ exercises/chaos.py
@@ -0,0 +1,7 @@
+def chaotic():
+    print("Chaotic function")
+    x = eval(input("Enter a number between 0 and 1"))
+    for i in range(10):
+        x = 3.9 * x * (1-x)
+        print(x)
+chaotic()
