b19381b2cb4a177215c765c1f24f752f74504af4
josemaia2002 <86317577+josemaia2002@users.noreply.github.com>
2021-06-22 21:37:44 -0300

    Create slope.py

---
+++ exercises/slope.py
@@ -0,0 +1,3 @@
+x1, y1, x2, y2 = eval(input("Enter the coordinates of two points: "))
+slope = (y2-y1) / (x2-x1)
+print("The slope of the line formed by the points (", x1, ",", y1, ") and (" , x2, ",", y2, ") is", slope)
