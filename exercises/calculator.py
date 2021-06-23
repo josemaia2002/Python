499a45a4a65fa81c7041908beabdac71df6f1c29
josemaia2002 <86317577+josemaia2002@users.noreply.github.com>
2021-06-22 21:35:38 -0300

    Create calculator.py

---
+++ exercises/calculator.py
@@ -0,0 +1,6 @@
+# calculator.py
+#   A program that works as a calculator
+
+for i in range(100):                                                        #Defines a for in which the calculation is done until the end of the loop
+    result = eval(input())                                                  #This statement performs the calculation and assign it to the variable 'result'
+    print(result)                                                           #Prints the result
