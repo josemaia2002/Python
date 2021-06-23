dcf68e343df42e6db289e90acf38726b1aa3b4b3
josemaia2002 <86317577+josemaia2002@users.noreply.github.com>
2021-06-22 21:37:31 -0300

    Create seriesAvg.py

---
+++ exercises/seriesAvg.py
@@ -0,0 +1,10 @@
+print("Sum of a natural numbers series")
+n = eval(input("Enter the number of elements: "))
+x = 0
+
+for i in range(n):
+    number = eval(input("Enter a number: "))
+    x = x + number
+
+avg = x / n
+print(avg)
